25926f4699bf58a2bf2ad7557368a522285f8a51
Aaron Finazzo <finazzocodes@gmail.com>
2023-10-10 19:37:25 -0400

    Generate a single round
    Implemented a function that generates a single round of rock, paper,
    scissors.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // get computer's random choice
 let getComputerChoice = () => {
-  let choices = ["rock", "paper", "scissors"];
+  const choices = ["rock", "paper", "scissors"];
   let randomChoice = Math.floor(Math.random() * 3);
   return (computerChoice = choices[randomChoice]);
 };
 
 // get players selection
-let getPlayerSelection = () => {
+let getUserChoice = () => {
   let userChoice = prompt("Please enter rock, paper, or scissors: ");
   userChoice = userChoice.toLowerCase();
   while (
@@ -22,5 +22,23 @@ let getPlayerSelection = () => {
 };
 
 // function to play a single round
+let playRound = (playerSelection, computerSelection) => {
+  console.log(playerSelection);
+  console.log(computerSelection);
+  if (playerSelection === computerSelection) {
+    console.log("It's a tie!");
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    console.log("Computer Wins!");
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    console.log("Computer Wins!");
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    console.log("Computer Wins!");
+  } else {
+    console.log("You win!");
+  }
+};
+const computerSelection = getComputerChoice();
+const playerSelection = getUserChoice();
+console.log(playRound(playerSelection, computerSelection));
 
 // function to generate a game that calls the single round multiple times
