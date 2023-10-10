09a551e47b678c5e78abfa26f7e43cf7f2b78cbb
Aaron Finazzo <finazzocodes@gmail.com>
2023-10-10 19:15:17 -0400

    Get computer's random choice
    Implemented function that gets the computer's random choice of rock,
    paper, or scissors.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("sup");
+// get computer's random choice
+let getComputerChoice = () => {
+  let choices = ["rock", "paper", "scissors"];
+  let randomChoice = Math.floor(Math.random() * 3);
+  return (computerChoice = choices[randomChoice]);
+};
+
+// get players selection
+
+// function to play a single round
+
+// function to generate a game that calls the single round multiple times
